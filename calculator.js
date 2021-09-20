a3b525ce95a8a643c6fd51a6cde905e872301813
Loumonth Jack <ljack@kisticket.com>
2021-09-20 00:40:09 -0500

    feature: input-validators

--- calculator.js
+++ calculator.js
@@ -17,3 +17,46 @@ const exit = (userInput) => {
 			return commandLine.close(); 
 	}
 }
+
+// Basic Arithmetic
+const operators = ["+", "-", "/", "*"] 
+const storedResponses = []
+
+// ----- Validators ----- //
+
+// Is Operator, if userInput is included in Operators Array 
+// Boolean
+const isOperator = (input) => {
+	return operators.includes(input)
+}; 
+// Is Operand, if userInput is a number 
+// Boolean
+const isOperand = (input)=> {
+	const number = parseInt(input);
+	return isNaN(number) == false ? true : false;
+};
+// Stored Responses is empty
+// Boolean
+const isEmpty = () => { 
+	return (storedResponses.length === 0 ? true : false ); 
+};
+// Stored Responses is Executable if has more than one number
+// Boolean
+const isExecutable = () => {
+	return (storedResponses.length > 1)
+};
+// is Invalid, if input is not Operand or Operator
+// Boolean
+const isInvalidInput = (userInput) => {
+	return isOperand(userInput) == false && isOperator(userInput) == false;
+};
+// is Invalid, if less than two values in Stored Responses 
+// Boolean
+const isInvalidStack = (userInput) => {
+	return isOperator(userInput) == true && isExecutable() == false;
+};
+// is Error when userInput is not Operand/Operator or Stored Responses has less than 2 values.
+// Boolean
+const isError = (userInput) => {
+	return (isInvalidInput(userInput) || isInvalidStack(userInput));
+};
