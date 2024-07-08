99d8ee53f21c2207315a5cf7db8c3f55e0e4a800
Loumonth <loumonth.jack.jr@gmail.com>
2024-07-07 20:44:33 -0500

    Update interface and storage

--- cli/tests/storage.test.ts
+++ cli/tests/storage.test.ts
@@ -2,48 +2,48 @@ import storage from '../storage';
 
 describe('Stack', () => {
     beforeEach(() => {
-        storage.resetSpace();
+        storage.reset();
     });
 
     test('resetSpace should empty the stack', () => {
-        storage.addItem(1);
-        storage.addItem(2);
-        expect(storage.resetSpace()).toEqual([]);
-        expect(storage.getSpace()).toEqual([]);
+        storage.add(1);
+        storage.add(2);
+        expect(storage.reset()).toEqual([]);
+        expect(storage.get()).toEqual([]);
     });
 
     test('addItem should add an item to the stack', () => {
-        expect(storage.addItem(5)).toBe(5);
-        expect(storage.getSpace()).toEqual([5]);
+        expect(storage.add(5)).toBe(5);
+        expect(storage.get()).toEqual([5]);
     });
 
     test('removeItem should remove and return the last item', () => {
-        storage.addItem(1);
-        storage.addItem(2);
-        expect(storage.removeItem()).toBe(2);
-        expect(storage.getSpace()).toEqual([1]);
+        storage.add(1);
+        storage.add(2);
+        expect(storage.remove()).toBe(2);
+        expect(storage.get()).toEqual([1]);
     });
 
     test('removeItem should return undefined for empty stack', () => {
-        expect(storage.removeItem()).toBeUndefined();
+        expect(storage.remove()).toBeUndefined();
     });
 
     test('isInvalid should return true for stack with 0 or 1 items', () => {
         expect(storage.isInvalid()).toBe(true);
-        storage.addItem(1);
+        storage.add(1);
         expect(storage.isInvalid()).toBe(true);
     });
 
     test('isInvalid should return false for stack with more than 1 item', () => {
-        storage.addItem(1);
-        storage.addItem(2);
+        storage.add(1);
+        storage.add(2);
         expect(storage.isInvalid()).toBe(false);
     });
 
     test('getSpace should return the current stack', () => {
-        storage.addItem(1);
-        storage.addItem(2);
-        storage.addItem(3);
-        expect(storage.getSpace()).toEqual([1, 2, 3]);
+        storage.add(1);
+        storage.add(2);
+        storage.add(3);
+        expect(storage.get()).toEqual([1, 2, 3]);
     });
 });
